96d2d1b2e93933eb6878aad2aa033008107279a9
Sara Tuma <saradavidtuma07@gmail.com>
2025-10-18 20:33:04 +0100

    feat: comment cors config

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,23 @@ import index from "./routes/index";
 dotenv.config();
 const app = express();
 const allowedOrigins = [
-  "http://localhost:3000",
+  "*",
   "https://seu-frontend.vercel.app",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(new Error("Not allowed by CORS"));
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
+app.use(cors()
+  // cors({
+  //   origin: (origin, callback) => {
+  //     if (!origin) return callback(new Error("Not allowed by CORS"));
+  //     if (allowedOrigins.includes(origin)) {
+  //       return callback(null, true);
+  //     } else {
+  //       return callback(new Error("Not allowed by CORS"));
+  //     }
+  //   },
+  //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  //   allowedHeaders: ["Content-Type", "Authorization"]
+  // })
 );
 
 app.use(express.json());
