4c8417401c02ca6542e456a329c0c605369b4c9d
Sara Tuma <saradavidtuma07@gmail.com>
2025-10-19 08:58:34 +0100

    feat: transctions update UserRole

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,6 +1,7 @@
 import {  Response } from "express";
 import { prisma } from "../config/prisma";
 import { AuthenticatedRequest } from "../middlewares/authenticate";
+import { UserRole } from "@prisma/client";
 
 export class TransactionController {
   static async findAll(req: AuthenticatedRequest, res: Response) {
@@ -9,7 +10,7 @@ export class TransactionController {
 
       let transactions;
 
-      if (req.user?.role === "ADMIN") {
+      if (req.user?.role === UserRole.ADMIN) {
         transactions = await prisma.transaction.findMany({
           orderBy: { createdAt: "desc" },
         });
