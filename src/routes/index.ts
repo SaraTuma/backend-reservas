f6fc20a049ec0c11452733f075140e2606b2e3c2
Sara Tuma <saradavidtuma07@gmail.com>
2025-10-19 01:42:52 +0100

    feat: user routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
+import userRoutes from "./user.routes";
 import serviceRoutes from "./service.routes";
 import reservationRoutes from "./reservation.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 router.use("/services", serviceRoutes);
 router.use("/reservations", reservationRoutes);
 
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { authenticate } from "../middlewares/authenticate";
+import { authorize } from "../middlewares/authorize";
+import { UserController } from "../controllers/user.controller";
+
+const router = Router();
+router.get("/", authenticate,authorize("findAllUser"), UserController.findAll);
+//router.put("/:id", authenticate, authorize("updateUser"), UserController.update);
+router.delete("/:id", authenticate, authorize("deleteUser"), UserController.delete);
+
+
+export default router;
